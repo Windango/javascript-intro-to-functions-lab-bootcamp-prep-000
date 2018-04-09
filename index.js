91c0a4c57a428eee6c59f92023308f683ff1dd01
robertlylegay@gmail.com <robertlylegay@gmail.com>
2018-04-09 03:11:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   } else if (string === lower) {
     return "I can't hear you!"
-  } else (lower = 'I love you, Grandma.') {
+  } else if (lower = 'I love you, Grandma.') {
     return "I love you, too"
   }
   
